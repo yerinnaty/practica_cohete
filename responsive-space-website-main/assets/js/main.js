b803ecbf8a603f141df1051baa4930db3f4170c9
yerinnaty <logiuratton@gmail.com>
2023-09-30 13:42:15 -0300

    a

---
+++ responsive-space-website-main/assets/js/main.js
@@ -0,0 +1,19 @@
+/*=============== SHOW MENU ===============*/
+const menu = document.getElementsById("nav-menu");
+const toggleButton = document.getElementsById("nav-toggle");
+const closeButton = document.getElementsById("na-close");
+
+if (toggleButton) {
+    toggleButton.addEventListener("click", ()=>{
+        menu.classList.add("show-menu")
+
+    })
+}
+if(closeButton)
+closeButton.addEventListener
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+
+/*=============== GSAP ANIMATION ===============*/
+
